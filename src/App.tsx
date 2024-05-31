b4d984d1b12d64c0ac879a014ea34d64f8fc0e65
AbdulSamman <sammanab@outlook.de>
2024-05-31 20:19:04 +0200

    burgermenu added

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,58 @@ import "./styles/App.scss";
 import { PageAboutUs } from "./pages/PageAboutUs";
 import { PageProducts } from "./pages/PageProducts";
 import { PageSupport } from "./pages/PageSupport";
-
 import { NavLink, Route, Routes, Navigate, Link } from "react-router-dom";
 import { Footer } from "./components/Footer";
 import { PageWelcome } from "./pages/PageWelcome";
+import { AiOutlineMenu } from "react-icons/ai";
+import { BsXLg } from "react-icons/bs";
+import { useState } from "react";
 
 export const App = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  const handleMenuOpen = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
-      <div className="App">
-        <div className="header">
-          <h1>
-            <Link to="/">Online Shop</Link>
-          </h1>
-          <nav>
-            <NavLink to={"/products"}>products</NavLink>
-            <NavLink to={"/support"}>support</NavLink>
-            <NavLink to={"/aboutUs"}>about us</NavLink>
+      <div
+        className="App"
+        onClick={() => {
+          isMenuOpen && setIsMenuOpen(false);
+        }}>
+        <div className="burgerMenu" onClick={handleMenuOpen}>
+          {!isMenuOpen ? (
+            <AiOutlineMenu className="menuIcon" />
+          ) : (
+            <BsXLg className="menuIcon" />
+          )}
+        </div>
+        <div className="row">
+          <NavLink to="/" className="logo">
+            <h1>Online Shop</h1>
+          </NavLink>
+          <nav className={`menu ${isMenuOpen ? "menuOpen" : ""}`}>
+            <div className="navRow">
+              <NavLink to={"/products"}>products</NavLink>
+              <NavLink to={"/support"}>support</NavLink>
+              <NavLink to={"/aboutUs"}>about us</NavLink>
+            </div>
           </nav>
         </div>
-
-        <Routes>
-          <Route path="/" element={<PageWelcome />} />
-          <Route path="/products" element={<PageProducts />} />
-          <Route path="/support" element={<PageSupport />} />
-          <Route path="/aboutUs" element={<PageAboutUs />} />
-          <Route path="*" element={<Navigate to={"/"} replace />} />
-        </Routes>
-
-        <>
-          <Footer />
-        </>
       </div>
+      <Routes>
+        <Route path="/" element={<PageWelcome />} />
+        <Route path="/products" element={<PageProducts />} />
+        <Route path="/support" element={<PageSupport />} />
+        <Route path="/aboutUs" element={<PageAboutUs />} />
+        <Route path="*" element={<Navigate to={"/"} replace />} />
+      </Routes>
+
+      <>
+        <Footer />
+      </>
     </>
   );
 };
